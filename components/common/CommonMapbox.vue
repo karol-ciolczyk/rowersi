d076b1ba815fe052f6214801d265a42c0d52803f
Karol Ciołczyk <dyweko@gmail.com>
2024-01-03 13:43:51 +0100

    link tags to the dead added in nuxt config file

--- components/common/CommonMapbox.vue
+++ components/common/CommonMapbox.vue
@@ -3,6 +3,7 @@ import { ref, onMounted, onUnmounted, watch } from "vue";
 import mapboxgl from "mapbox-gl";
 import directionsStyle from "~/constants/directions-style";
 
+let directions: any = null;
 let mapContainer = ref(null);
 mapboxgl.accessToken =
   "pk.eyJ1Ijoia2FyY2lvIiwiYSI6ImNrcTd6YjExejAxc3kyb3BrcnBzY252em4ifQ.emytj-LkRX7RcGueM2S9HA";
@@ -11,8 +12,9 @@ let map: null | any = null;
 onMounted(() => {
   import("@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions").then(
     (module) => {
+      console.log(directionsStyle);
       const MapboxDirections = module.default;
-      const directions = new MapboxDirections({
+      const drs = new MapboxDirections({
         accessToken: mapboxgl.accessToken,
         profile: "mapbox/cycling",
         unit: "metric",
@@ -39,7 +41,9 @@ onMounted(() => {
       map.addControl(new mapboxgl.FullscreenControl(), "bottom-left");
       const nav = new mapboxgl.NavigationControl();
       map.addControl(nav, "bottom-left");
-      map.addControl(directions, "top-left");
+      map.addControl(drs, "top-left");
+
+      directions = drs;
     },
   );
 });
@@ -49,10 +53,51 @@ onUnmounted(() => {
     map = null;
   }
 });
+
+const sampleRoutePoints = {
+  origin: [19.411739, 50.141413],
+  destination: [19.526844, 50.100241],
+  destinationName:
+    "Tenczyńska 86, 32-566 Nieporaz, Lesser Poland Voivodeship, Poland",
+};
+function addRoute(routePoints, waypoints) {
+  directions.removeRoutes(); // must be here to prevent duplicating waypoints
+
+  if (directions._map?._markers[0]) directions._map._markers[0].remove();
+  if (routePoints.origin) {
+    directions.setOrigin(routePoints.origin);
+    const el = document.createElement("div");
+    el.className = "originElement";
+    const coordinates = routePoints.origin;
+    new mapboxgl.Marker(el)
+      .setLngLat(coordinates)
+      .setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>"))
+      .addTo(map.current);
+  }
+  if (routePoints.destination) {
+    directions.setDestination(routePoints.destination);
+    const coordinates = routePoints.destination;
+    new mapboxgl.Marker({
+      color: "red",
+    })
+      .setLngLat(coordinates)
+      .setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>"))
+      .addTo(map.current);
+  }
+  // if (Object.keys(waypoints).length > 0) {
+  //   const waypointNumbers = Object.keys(waypoints);
+  //   waypointNumbers.forEach((number) => {
+  //     const coordinates = waypoints[number];
+  //     const figure = Number(number);
+  //     directions.addWaypoint(figure, coordinates);
+  //   });
+  // }
+}
 </script>
 
 <template>
   <div ref="mapContainer" class="map-container" />
+  <v-btn @click="addRoute(sampleRoutePoints, {})">Add Route</v-btn>
 </template>
 
 <style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,4 +23,19 @@ export default defineNuxtConfig({
       },
     },
   },
+  app: {
+    head: {
+      link: [
+        {
+          href: "https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css",
+          rel: "stylesheet",
+        },
+        {
+          href: "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css",
+          rel: "stylesheet",
+          type: "text/css",
+        },
+      ],
+    },
+  },
 });
